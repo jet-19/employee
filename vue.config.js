e53abf4ee78a6747f431207fc673a6fa7279b6dc
jet-19 <13586217736@139.com>
2025-07-30 17:50:10 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    port: 8080,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }
+    }
+  },
+  lintOnSave: false
+}) 
